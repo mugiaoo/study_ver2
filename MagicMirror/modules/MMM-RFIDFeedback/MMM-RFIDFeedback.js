bf9db64c59cb80163a3736ca3b3e8b588dc54b54
mugiaoo <b1020191@fun.ac.jp>
2025-07-29 22:58:50 +0900

    MMファイル書き換え

--- MagicMirror/modules/MMM-RFIDFeedback/MMM-RFIDFeedback.js
+++ MagicMirror/modules/MMM-RFIDFeedback/MMM-RFIDFeedback.js
@@ -1,11 +1,18 @@
-Module.register("MMM-RFIDFeedback", {
+Module.register("MMM-FeedbackDisplay", {
   defaults: {
-    duration: 10000
+    duration: 10000,
+    endpoint: "http://localhost:8000/feedback",
+    updateInterval: 10000  // デフォルト更新間隔
   },
 
   start: function () {
     this.message = null;
     this.hideTimer = null;
+
+    this.getFeedback();  // 初回実行
+    setInterval(() => {
+      this.getFeedback();  // 一定間隔で更新
+    }, this.config.updateInterval);
   },
 
   getDom: function () {
@@ -16,16 +23,28 @@ Module.register("MMM-RFIDFeedback", {
     return wrapper;
   },
 
-  socketNotificationReceived: function (notification, payload) {
-    if (notification === "SHOW_FEEDBACK") {
-      this.message = payload;
-      this.updateDom();
+  getFeedback: function () {
+    fetch(this.config.endpoint)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();  // Flask からは JSON が返る前提
+      })
+      .then(data => {
+        if (data && data.message) {
+          this.message = data.message;
+          this.updateDom();
 
-      if (this.hideTimer) clearTimeout(this.hideTimer);
-      this.hideTimer = setTimeout(() => {
-        this.message = null;
-        this.updateDom();
-      }, this.config.duration);
-    }
+          if (this.hideTimer) clearTimeout(this.hideTimer);
+          this.hideTimer = setTimeout(() => {
+            this.message = null;
+            this.updateDom();
+          }, this.config.duration);
+        }
+      })
+      .catch(error => {
+        console.error("[送信エラー] フィードバック取得中に例外発生:", error);
+      });
   }
 });
